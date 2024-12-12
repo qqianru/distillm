4b7099f76c3c9083f42d9efd6b60b4968b2581dc
qqianru <124508660+qqianru@users.noreply.github.com>
2024-12-12 09:47:31 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 export NCCL_DEBUG=""
 # conda install pytorch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 pytorch-cuda=12.1 -c pytorch -c nvidia
 # pip install transformers==4.42.4
-pip install vllm==0.5.0
+pip install vllm --upgrade
 pip install deepspeed
 pip install nltk
 pip install numerize
