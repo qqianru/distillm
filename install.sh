f8d57835f8f1a9e3ee889f49e320621879b8a90e
qqianru <124508660+qqianru@users.noreply.github.com>
2024-12-12 10:01:18 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 export NCCL_DEBUG=""
 pip install torch==2.5.1+cu121 torchvision==0.20.1+cu121 torchaudio==2.5.1+cu121 --index-url https://download.pytorch.org/whl/cu121
-pip install transformers==4.42.4
+pip install transformers==4.45.2
 pip install vllm==0.6.4.post1
 pip install deepspeed
 pip install nltk
