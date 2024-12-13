96466714c7c67dbbba82cb1b89591cf246f526b6
qqianru <124508660+qqianru@users.noreply.github.com>
2024-12-13 07:30:46 -0500

    Update process_data_pretrain.sh

--- scripts/gpt2/tools/process_data_pretrain.sh
+++ scripts/gpt2/tools/process_data_pretrain.sh
@@ -5,7 +5,7 @@ MAX_LENGTH=512
 PYTHONPATH=${BASE_PATH} python3 ${BASE_PATH}/tools/process_data_pretrain.py \
     --data-dir ${BASE_PATH}/data/openwebtext \
     --processed-data-dir ${BASE_PATH}/processed_data/openwebtext/gpt2/${MAX_LENGTH}/ \
-    --model-path ${BASE_PATH}/checkpoints/gpt2-large \
+    --model-path ${BASE_PATH}/checkpoints/gpt2 \
     --max-length ${MAX_LENGTH} \
     --train-num 10000000 \
     --data-process-workers 32 \
