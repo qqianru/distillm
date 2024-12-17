2bf8c19b23574b420a7e6fd49d61f36e848707ff
qqianru <124508660+qqianru@users.noreply.github.com>
2024-12-17 08:33:24 -0500

    Update kd_base.sh

--- scripts/gpt2/kd/kd_base.sh
+++ scripts/gpt2/kd/kd_base.sh
@@ -46,7 +46,7 @@ OPTS+=" --n-gpu ${GPUS_PER_NODE}"
 # OPTS+=" --gradient-checkpointing"
 # data
 OPTS+=" --data-dir ${DATA_DIR}"
-OPTS+=" --num-workers 1"
+OPTS+=" --num-workers 0"
 OPTS+=" --dev-num 1000"
 # hp
 OPTS+=" --lr ${LR}"
